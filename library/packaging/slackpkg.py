c6a3f9fb8c4ab1c71793ae0923e91fc354e601e4
Bini R <bini.r@initcron.org>
2014-03-13 17:10:41 +0530

    Modified slackware installation code

--- library/packaging/slackpkg.py
+++ library/packaging/slackpkg.py
@@ -1,33 +1,46 @@
-!/usr/bin/python
+#!/usr/bin/python
 
 import re
 import shlex
 import syslog
 
-path = '/root/Downloads'
+path = '/root/Downloads/'
+
 
 # Function used for executing commands.
 def execute_command(cmd, module):
-    if debug:
+   # if debug:
         syslog.syslog("execute_command(): cmd = %s" % cmd)
     # Break command line into arguments.
     # This makes run_command() use shell=False which we need to not cause shell
     # expansion of special characters like '*'.
-    cmd_args = shlex.split(cmd)
-    return module.run_command(cmd_args)
+        cmd_args = shlex.split(cmd)
+        return module.run_command(cmd_args)
 
 
 
-# Function used to make sure a package is present.
-def package_present(name, installed_state, pkg_spec, module):
+# Function used to install package.
+def package_present(name, module):
+
+        install_cmd = 'upgradepkg'
 
         # Install the package
-        (rc, stdout, stderr) = execute_command("%s %s" % (install_cmd, name), module)
+        (rc, stdout, stderr) = execute_command("%s %s %s" % (install_cmd, path, name), module)
 
-         rc = 1
+        # rc=1
 
-    return (rc, stdout, stderr, changed)
+        return (rc, stderr)
 
+
+#        rc, stdout, stderr = execute_command(command, module)
+
+        if (stderr):
+              module.fail_json(msg="failed in get_package_state(): " + stderr)
+
+        if rc == 0:
+              return True
+        else:
+              return False
 
 
 # Main control flow
@@ -38,7 +51,7 @@ def main():
             name = dict(required=True),
             state = dict(required=True, choices=['present']),
         ),
-        supports_check_mode = True
+#        supports_check_mode = True
     )
 
     name = module.params['name']
@@ -47,6 +60,7 @@ def main():
     rc = 0
     stdout = ''
     stderr = ''
+    dpath = ''
     result = {}
     result['name'] = name
     result['state'] = state
@@ -54,20 +68,32 @@ def main():
 
 
     # Parse package name and put results in the pkg_spec dictionary.
-    pkg_spec = {}
-    parse_package_name(name, pkg_spec, module)
+#    pkg_spec = {}
+#    parse_package_name(name, pkg_spec, module)
 
+
+    # Get package state.
+#    installed_state = get_package_state(name, pkg_spec, module)
 
 
     # Perform requested action.
     if state in ['present']:
-        (rc, stdout, stderr, changed) = package_present(name, installed_state, pkg_spec, module)
-    
+         (rc, changed)=package_present(name, module)
 
-   
+    if rc != 0:
+        if stderr:
+            module.fail_json(msg=stderr)
+        else:
+            module.fail_json(msg=stdout)
+
+    result['changed'] = changed
+
     module.exit_json(**result)
 
 # import module snippets
 from ansible.module_utils.basic import *
 main()
+
+
+
 
